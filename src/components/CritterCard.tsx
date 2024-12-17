256e6219325b48ab6c682fbdba3b1ace5b4244c7
GooglyBlox <53668973+GooglyBlox@users.noreply.github.com>
2024-12-17 06:13:36 -0800

    fix: card icon spacing

--- src/components/CritterCard.tsx
+++ src/components/CritterCard.tsx
@@ -64,7 +64,7 @@ export function CritterCard({ critter, currentHemisphere, onCaughtChange }: Crit
     >
       <CardHeader className="pt-6 pb-4">
         <div className="flex items-start gap-3">
-          <div className="p-2 bg-white rounded-full">
+          <div className="p-1 bg-white rounded-full">
             <img 
               src={getIconPath()} 
               alt={`${critter.name} icon`}
